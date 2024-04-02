839ed930512058cab5057b00905a1850410241a2
Leidy Navarro <liderdesarrollo@ttncompany.com>
2024-04-02 15:38:06 -0500

    feat!: create configuration file

---
+++ configuration/environment.py
@@ -0,0 +1,73 @@
+""" configurations settings per environment """
+
+import os
+import dataclasses
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+@dataclasses.dataclass
+class Config:
+    """Base configuration"""
+
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    DEBUG = False
+
+
+@dataclasses.dataclass
+class DevelopmentConfig(Config):
+    """Development configuration"""
+
+    DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    HOST = os.getenv("POSTGRES_HOST")
+    USER = os.getenv("POSTGRES_USER")
+    PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    DBPORT = os.getenv("DBPORT")
+    DATABASE = os.getenv("POSTGRES_DATABASE")
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{USER}:{PASSWORD}@{HOST}:{DBPORT}/{DATABASE}"
+    )
+
+
+@dataclasses.dataclass
+class TestingConfig(Config):
+    """Testing configuration"""
+
+    DEBUG = True
+    TESTING = True
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    HOST = None
+    USER = None
+    PASSWORD = None
+    DBPORT = None
+    DATABASE = None
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{USER}:{PASSWORD}@{HOST}:{DBPORT}/{DATABASE}"
+    )
+
+
+@dataclasses.dataclass
+class ProductionConfig(Config):
+    """Production configuration"""
+
+    DEBUG = False
+    HOST = None
+    USER = None
+    PASSWORD = None
+    DBPORT = None
+    DATABASE = None
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{USER}:{PASSWORD}@{HOST}:{DBPORT}/{DATABASE}"
+    )
+
+
+config_by_name = {
+    "dev": DevelopmentConfig,
+    "test": TestingConfig,
+    "prod": ProductionConfig,
+}
+
+key = Config.SECRET_KEY
